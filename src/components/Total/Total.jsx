03827978a186434c8861a1d4d45d0aa1b237ebb6
Eivind Haukeberg <e.haukeberg@gmail.com>
2025-05-24 22:30:42 +0200

    add Total.jsx - add total.css

---
+++ src/components/Total/Total.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Total.css';
+
+const Total = ({ expenses }) => {
+  const total = expenses.reduce((sum, e) => sum + e.amount, 0);
+  return <h2 className='total__value'>Total: ${total.toFixed(2)}</h2>;
+};
+
+export default Total;
